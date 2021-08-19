54e49cde9bb98174589639126007937f4d53288e
Zhuoqin <lanzhuoqin@gmail.com>
2021-08-18 20:34:03 -0400

    changed testA.

--- testA.js
+++ testA.js
@@ -1,1 +1,13 @@
-var test = 1
+var test = 1;
+
+var testAFuc = () => {
+    console.log('Hello world');
+}
+
+var testBFuc = () => {
+    console.log('Hello world');
+}
+
+var testCFuc = () => {
+    console.log('Hello world');
+}
