720aec10981fd85f574ec5756771d073e19b90f9
Zhuoqin <lanzhuoqin@gmail.com>
2021-08-18 20:47:17 -0400

    fdsadfas

--- testA.js
+++ testA.js
@@ -1,14 +1,11 @@
 var test = 1
 var test2 = 2
-
 var testAFuc = () => {
     console.log('Hello world');
 }
-
 var testBFuc = () => {
     console.log('Hello world');
 }
-
 var testCFuc = () => {
     console.log('Hello world');
 }
